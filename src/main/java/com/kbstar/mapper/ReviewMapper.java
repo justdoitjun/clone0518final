ca3e67a9a17c9c59dfa5f4eeae34869c350c04cd
junstdoitjun <bing74700@gmail.com>
2023-05-19 13:21:48 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/mapper/ReviewMapper.java
@@ -0,0 +1,14 @@
+package com.kbstar.mapper;
+
+import com.kbstar.dto.Review;
+import com.kbstar.frame.KBMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface ReviewMapper extends KBMapper<String, Review> {
+    public void deleteall();
+    public void reviveall();
+    public void updateisHidden(Integer i);
+}
